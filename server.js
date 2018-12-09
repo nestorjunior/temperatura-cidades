3b980bfdd08c0acf3d287855363ba7fffcdc9a04
Nestor Pereira <nestorpereira@MacBook-de-Nestor.local>
2018-12-09 18:54:45 -0200

    corrigindo script.js + incluindo path no arquivo: server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express')
 var app = express()
+var path = require('path')
  
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/style.css', function(req, res) {
+    res.sendFile(path.join(__dirname + '/style.css'));
+});
+
+app.get('/script.js', function(req, res) {
+    res.sendFile(path.join(__dirname + '/script.js'));
 });
  
 app.listen(3000)
